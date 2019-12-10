e1be2d003cd0fe88bd5683105531bc354ed4adeb
silencer903 <zapara04@rambler.ru>
2019-12-11 03:33:42 +0500

    Сборка приложения

---
+++ precache-manifest.7e6d77c473c0592504a22a5f1f542d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb37f684fafc938946f0",
+    "url": "/vue-inputs-app/css/app.daa445e4.css"
+  },
+  {
+    "revision": "ce2ada183aa1356b666ab42e46ca8042",
+    "url": "/vue-inputs-app/index.html"
+  },
+  {
+    "revision": "fb37f684fafc938946f0",
+    "url": "/vue-inputs-app/js/app.aa975cf9.js"
+  },
+  {
+    "revision": "cb9add7cc75404013d1c",
+    "url": "/vue-inputs-app/js/chunk-vendors.95b3df12.js"
+  },
+  {
+    "revision": "9c8e5236fa9580a909f58877b9abb5e4",
+    "url": "/vue-inputs-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-inputs-app/robots.txt"
+  }
+]);
